37c898c0c11648e2698c796749317ed4f5b3c15c
Bjoern Strobach <bstrobach@mrappware.de>
2019-07-22 23:55:56 +0200

    Example Todo application

---
+++ workspace/loopback4-example-todo/dist/application.d.ts
@@ -0,0 +1,7 @@
+import { ApplicationConfig } from '@loopback/core';
+import { RestApplication } from '@loopback/rest';
+declare const TodoListApplication_base;
+export declare class TodoListApplication extends TodoListApplication_base {
+    constructor(options?: ApplicationConfig);
+}
+export {};
